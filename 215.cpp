bec1e02dd440f537534a0468df9670eb917394b4
Krishnamoorthi P L <117007783+kr1shnam00rthi@users.noreply.github.com>
2024-08-22 09:28:28 +0530

    Delete 215.cpp

--- 215.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        auto it=nums.end()-k;
-        return *(it);     
-    }
-};
